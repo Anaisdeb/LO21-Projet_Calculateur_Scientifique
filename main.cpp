55442fcd55eafe29b05a048533e025df2b8d9ce7
leCodeurDu60 <72913987+leCodeurDu60@users.noreply.github.com>
2021-01-02 17:01:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,18 +16,3 @@ int main(int argc, char* argv[])
 
     return app.exec();
 }
-
-/* ===================== A FAIRE =====================
-    * si la taille de la littérale dépasse la taille de la pile, mettre ". . ." à la fin de la ligne
-    * vérification du contenu des QLineEdit pour la modification des var/prog nécessaire ?
-*/
-
-//  methode pour commenter
-/**
-* @brief : (à quoi sert ma méthode) : ajouter un élement de type Evt à mon vector
-* @param : (paramètres de la fonction) référence vers l'EVT à ajouter
-* @return : (valeur de retour) Agenda&, une référence vers l'agenda courant (this)
-*/
-
-
-
